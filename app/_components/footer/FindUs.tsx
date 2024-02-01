dca28212ab2974f8d5d8be29417d1b768389aef4
Lee <leesheppard2404@gmail.com>
2024-02-01 15:40:47 +0100

    refactor: Remove unused import and adjust styling in FindUs component

--- app/_components/footer/FindUs.tsx
+++ app/_components/footer/FindUs.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { SlLocationPin } from 'react-icons/sl';
 
 const FindUs = () => {
   return (
     <section>
       <div className="footer-heading">
-        <SlLocationPin />
-        <h5 className="ml-3">Find Us</h5>
+        <h5>Find Us</h5>
       </div>
       <a
-        className="footer-text underline"
+        className="footer-text underline ml-4"
         href="https://maps.app.goo.gl/3rH2jLrK8ed4ZN1f9"
       >
         Hornsgatan 29B, Stockholm, 118 49
