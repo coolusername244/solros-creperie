2a64f8904d2014845c65a3d73cdcaf62a62ca6e4
Lee <leesheppard2404@gmail.com>
2024-02-01 15:41:47 +0100

    refactor: each footer component exported into individual component files

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import { CiLocationOn } from 'react-icons/ci';
-import { TfiEmail } from 'react-icons/tfi';
-import { FiPhone } from 'react-icons/fi';
-import { FaInstagram } from 'react-icons/fa';
-import { FaRegClock } from 'react-icons/fa';
+import OpeningHours from './OpeningHours';
+import FindUs from './FindUs';
+import ContactUs from './ContactUs';
+import Social from './Social';
 
 const Footer = () => {
   return (
-    <footer className="bg-green-900 text-white flex flex-col items-center p-12">
-      <p>Opening Times</p>
-      <FaRegClock />
-      <p>Monday - Friday</p>
-      <p>0800 - 2000</p>
-      <p>Saturday</p>
-      <p>0900 - 1800</p>
-      <p>Sunday</p>
-      <p>1000 - 1700</p>
-      <p>Find Us</p>
-      <CiLocationOn />
-      <p>Hornsgatan 29B, Stockholm, 118 49</p>
-      <p>Contact Us</p>
-      <TfiEmail />
-      <p>info@solroscreperie.se</p>
-      <FiPhone />
-      <p>+46700415015</p>
-      <p>Social Media</p>
-      <FaInstagram />
-      <p>instagram</p>
+    <footer className="footer-component">
+      <OpeningHours />
+      <FindUs />
+      <ContactUs />
+      <Social />
     </footer>
   );
 };
