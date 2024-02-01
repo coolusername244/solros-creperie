4426bc59131efcdd23a51a164b1d2b32c3938772
Lee <leesheppard2404@gmail.com>
2024-02-01 14:14:00 +0100

    chore: new types added for OpenTime, ContactMethod, and SocialLink

--- app/_types/types.ts
+++ app/_types/types.ts
@@ -2,3 +2,20 @@ export type Navlink = {
   name: string;
   path: string;
 };
+
+export type OpenTime = {
+  day: string;
+  times: string;
+};
+
+export type ContactMethod = {
+  method: string;
+  icon: JSX.Element;
+  contact: string;
+};
+
+export type SocialLink = {
+  name: string;
+  icon: JSX.Element;
+  link: string;
+};
