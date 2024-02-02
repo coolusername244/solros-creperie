ff44f9b0f3482e05e6a3585143f7b61810e21098
Lee <leesheppard2404@gmail.com>
2024-02-02 20:10:56 +0100

    feat: Add translation support for OpeningHours component

--- app/_components/footer/OpeningHours.tsx
+++ app/_components/footer/OpeningHours.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { OpenTime } from '@/_types/types';
+import { useTranslations } from 'next-intl';
 
 const openTimes: OpenTime[] = [
   {
-    day: 'Monday - Friday',
+    day: 'monday-friday',
     times: '08:00 - 20:00',
   },
   {
-    day: 'Saturday',
+    day: 'saturday',
     times: '09:00 - 18:00',
   },
   {
-    day: 'Sunday',
+    day: 'sunday',
     times: '10:00 - 17:00',
   },
 ];
 
 const OpeningHours = () => {
+  const t = useTranslations('footer.open-hours');
+
   return (
     <section className="footer-section lg:w-[100%] lg:text-center">
       <div className="footer-heading lg:justify-center">
-        <h5>Opening Hours</h5>
+        <h5>{t('title')}</h5>
       </div>
       <div className="footer-text">
         {openTimes.map((time, i) => (
           <>
-            <p key={i}>{time.day}</p>
+            <p key={i}>{t(`days.${time.day}`)}</p>
             <p className="mb-3">{time.times}</p>
           </>
         ))}
