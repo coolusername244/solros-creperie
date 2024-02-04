3446398ba16f13dbd30dadb3c04ad67f47e4065a
Lee <leesheppard2404@gmail.com>
2024-02-04 17:07:23 +0100

    feat: made language selector options dynamic based on current locale

--- app/_components/navbar/LanguageSelector.tsx
+++ app/_components/navbar/LanguageSelector.tsx
@@ -2,12 +2,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useLocale } from 'next-intl';
 import { GB, SE, UA } from 'country-flag-icons/react/3x2';
+import { locales } from '@/_types/types';
 
 const LanguageSelector = () => {
   const [languagesShown, setLanguagesShown] = useState<boolean>(false);
   const languageSelectorRef = useRef<HTMLDivElement>(null);
 
-  const locale = useLocale();
+  const userLocale = useLocale();
 
   const handleClickOutside = (e: MouseEvent) => {
     if (
@@ -25,9 +26,27 @@ const LanguageSelector = () => {
     };
   }, []);
 
+  const locales: locales[] = [
+    {
+      locale: 'sv',
+      language: 'Svenska',
+      flag: <SE height={15} />,
+    },
+    {
+      locale: 'en',
+      language: 'English',
+      flag: <GB height={15} />,
+    },
+    {
+      locale: 'ua',
+      language: 'Український',
+      flag: <UA height={15} />,
+    },
+  ];
+
   let flag: JSX.Element;
 
-  switch (locale) {
+  switch (userLocale) {
     case 'sv':
       flag = <SE />;
       break;
@@ -61,9 +80,9 @@ const LanguageSelector = () => {
             fill="currentColor"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </button>
@@ -74,24 +93,21 @@ const LanguageSelector = () => {
         } right-0 z-10 mt-2 w-auto p-1 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5`}
       >
         <div className="py-1" role="none">
-          <a
-            href="/"
-            className="flex items-center text-gray-700 px-4 py-2 text-sm"
-          >
-            <span className="mr-2">
-              <SE height={15} />
-            </span>{' '}
-            Swedish
-          </a>
-          <a
-            href="/"
-            className="flex items-center text-gray-700 px-4 py-2 text-sm"
-          >
-            <span className="mr-2">
-              <UA height={15} />
-            </span>{' '}
-            Ukrainian
-          </a>
+          {locales.map((l, i) => {
+            if (l.locale !== userLocale) {
+              return (
+                <a
+                  key={i}
+                  href={`/${l.locale}`}
+                  className="flex items-center text-gray-700 px-4 py-2 text-sm"
+                >
+                  <span className="mr-2">{l.flag}</span> {l.language}
+                </a>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     </div>
