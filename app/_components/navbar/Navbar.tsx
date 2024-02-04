d834457f3d54d2b4f1d191ddf6b847be3b3d29d3
Lee <leesheppard2404@gmail.com>
2024-02-04 15:48:13 +0100

    feat: Add LanguageSelector component to Navbar

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Navlinks from './Navlinks';
+import LanguageSelector from './LanguageSelector';
 import { GiSunflower } from 'react-icons/gi';
 
 const Navbar = () => {
@@ -13,22 +14,25 @@ const Navbar = () => {
           <GiSunflower />
         </a>
       </div>
-      <div className="flex items-center lg:hidden">
-        <button
-          className="flex"
-          onClick={() => {
-            setNavItemsShown(!navItemsShown);
-          }}
-        >
-          <svg
-            className="fill-current h-5 w-5"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      <div className="flex">
+        <div className="flex items-center lg:hidden">
+          <LanguageSelector />
+          <button
+            className="flex"
+            onClick={() => {
+              setNavItemsShown(!navItemsShown);
+            }}
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <svg
+              className="fill-current h-5 w-5"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div
         className={`${
