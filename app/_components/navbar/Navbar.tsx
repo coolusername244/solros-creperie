92284b2acb892e76aa3e287565b5f9bf34a7a208
Lee <leesheppard2404@gmail.com>
2024-02-07 13:17:44 +0100

    feat: added language selector to both mobile and desktop and refactored a tag to Link tag

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+
 import Navlinks from './Navlinks';
 import LanguageSelector from './LanguageSelector';
 import SunFlower from '@/_assets/logo-no-background-no-text.png';
@@ -11,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between flex-wrap bg-primary text-white p-6">
       <div className="flex mr-6">
-        <a href="/" className="text-4xl">
+        <Link href="/">
           <Image
             src={SunFlower}
             alt="Sunflower"
@@ -19,33 +21,36 @@ const Navbar = () => {
             height={500}
             className="h-12 w-12"
           />
-        </a>
+        </Link>
       </div>
-      <div className="flex">
-        <div className="flex items-center lg:hidden">
+      <div className="flex items-center">
+        <div className="flex mr-4 lg:hidden">
           <LanguageSelector />
-          <button
-            className="flex"
-            onClick={() => {
-              setNavItemsShown(!navItemsShown);
-            }}
-          >
-            <svg
-              className="fill-current h-5 w-5"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
         </div>
+        <button
+          className="flex lg:hidden"
+          onClick={() => {
+            setNavItemsShown(!navItemsShown);
+          }}
+        >
+          <svg
+            className="fill-current h-5 w-5"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
       </div>
       <div
         className={`${
           navItemsShown ? 'inline' : 'hidden'
         } w-full lg:flex lg:items-center lg:w-auto`}
       >
+        <div className="sm:hidden lg:flex">
+          <LanguageSelector />
+        </div>
         <Navlinks />
       </div>
     </nav>
