19106314276c0a7107699a50d5c2b4f65d9446b2
Lee <leesheppard2404@gmail.com>
2024-02-10 18:37:41 +0100

    feat: Add image and translation to NotFound component

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,12 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+
+import SunFlower from '@/_assets/site-icons/logo-no-background-no-text.png';
 
 export default function NotFound() {
+  const t = useTranslations('not-found');
   return (
-    <div>
-      <h1>Uhoh!</h1>
+    <main className="screen-content flex flex-col justify-center items-center">
+      <h1 className="text-2xl">{t('heading')}</h1>
+      <Image
+        src={SunFlower}
+        alt="Sunflower"
+        width={500}
+        height={500}
+        className="h-96 w-96 my-8"
+      />
       <div>
-        <Link href="/">Go back to Home</Link>
+        <Link href="/">
+          <button className="text-xl border border-primary bg-primary font-medium p-3 rounded-xl text-white">
+            {t('button')}
+          </button>
+        </Link>
       </div>
-    </div>
+    </main>
   );
 }
