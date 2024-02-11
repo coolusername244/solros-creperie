17aca633ea18e0b6a140548d8b287f1799b644fb
Lee <leesheppard2404@gmail.com>
2024-02-11 19:37:11 +0100

    feat: added home screen background image with overlay

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <main className="screen-content">Home page</main>;
+  return (
+    // <main className="bg-cover bg-center screen-content home-image">
+    //   <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50"></div>
+    // </main>
+    <main className="flex min-h-[calc(100vh-96px)] justify-center items-center flex-col">
+      <div className="w-full min-h-[calc(100vh-96px)] bg-[url('_assets/cafe/cafe.jpg')] bg-cover bg-center">
+        <div
+          className="w-full min-h-[calc(100vh-96px)] flex  justify-center items-center 
+             bg-secondary/30 "
+        >
+          <h1 className="text-6xl font-bold text-white">Home page</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
