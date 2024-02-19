7ed8508fe07a429cb6db56140e96cf4aa5b4a80e
Lee <leesheppard2404@gmail.com>
2024-02-19 19:31:10 +0100

    refactor: updated asset file structure

---
+++ app/_assets/datasets/data.tsx
@@ -0,0 +1,28 @@
+import { Navlink, SocialLink } from '@/_types/types';
+import { FaInstagram } from 'react-icons/fa';
+
+export const socialLinks: SocialLink[] = [
+  {
+    name: 'Instagram',
+    icon: <FaInstagram />,
+    link: 'https://www.instagram.com/solroscreperie',
+  },
+];
+
+export const navlinks: Navlink[] = [
+  {
+    name: 'menu',
+  },
+  {
+    name: 'offers',
+  },
+  {
+    name: 'news',
+  },
+  {
+    name: 'about',
+  },
+  {
+    name: 'contact',
+  },
+];
