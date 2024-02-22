486e66361f8683620ccbd09e8439a0e71362fa6d
Lee <leesheppard2404@gmail.com>
2024-02-22 19:47:42 +0100

    refactor: Add insta handle to desktop

--- app/_components/navbar/Social.tsx
+++ app/_components/navbar/Social.tsx
@@ -5,8 +5,11 @@ import { SocialLink } from '@/_types/types';
 const Social: FC<SocialLink> = ({ name, icon, link }) => {
   return (
     <button key={name} className="navbar-link text-2xl">
-      <Link target="_blank" href={link}>
-        {icon}
+      <Link target="_blank" className="flex items-center" href={link}>
+        {icon}{' '}
+        <span className="hidden lg:flex ml-1 text-sm lowercase">
+          solroscreperie
+        </span>
       </Link>
     </button>
   );
