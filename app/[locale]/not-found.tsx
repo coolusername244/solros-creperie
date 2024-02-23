3a8a092671605ecc0e2e1bfaee67f5f4cc7eae9c
Lee <leesheppard2404@gmail.com>
2024-02-23 17:18:39 +0100

    Remove SunFlower image from NotFound component

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -2,20 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 
-import SunFlower from '@/_assets/site-icons/logo-no-background-no-text.png';
-
 export default function NotFound() {
   const t = useTranslations('not-found');
   return (
     <main className="screen-content flex flex-col justify-center items-center">
       <h1 className="text-2xl">{t('heading')}</h1>
-      <Image
-        src={SunFlower}
-        alt="Sunflower"
-        width={500}
-        height={500}
-        className="h-96 w-96 my-8"
-      />
       <div>
         <Link href="/">
           <button className="text-xl border border-primary bg-primary font-medium p-3 rounded-xl text-white">
