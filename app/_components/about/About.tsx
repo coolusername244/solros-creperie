65e9617f6ec263fa868bb0caacaa12fc83e2b521
Lee <leesheppard2404@gmail.com>
2024-02-24 17:36:25 +0100

    feat: Refactor About component to use translations

--- app/_components/about/About.tsx
+++ app/_components/about/About.tsx
@@ -1,44 +1,19 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
+
+import { about } from '@/_assets/datasets/data';
 
 const About = () => {
+  const t = useTranslations('about');
   return (
-    <div className="screen-content">
-      <h2>about</h2>
-      <p>Om oss</p>
-      <p>Välkommen till Solros Crêperie!</p>
-      <p>
-        Vårt kafé öppnade dörrarna i november 2023, efter att vi genomgått en
-        omfattande renovering i lokalen där varje enskild detalj noggrant
-        förnyades. Från väggarna till golven och inredningen, allt bär prägeln
-        av vår egen idé och kreativitet.
-      </p>
-      <p>
-        Vi önskar att besökare väljer att stanna i en avslappnad atmosfär hos
-        oss och återkoppla med en välsmakande kopp kaffe.
-      </p>
-      <p>
-        Vårt främsta mål är att erbjuda gästerna det bästa kaffet, därför föll
-        valet på Johan & Nyström. Vi provsmakade flera sorter vid besök på
-        bryggeriet och valde Bold Statement för bryggkaffe med smaker av
-        choklad, hasselnötter och körsbär.
-      </p>
-      <p>
-        Vår älskade La Marzocco-espressomaskin, en handgjord italiensk
-        följeslagare, skapar kaffe från Bob o Link-bönor med smaker av choklad,
-        kola och söta plommon. Våra maträtter, galetter och crêpes, förmedlar
-        fransk matkultur och skapar en känsla av exklusivitet till vardagen.
-      </p>
-      <p>
-        De är fyllda med noggrant utvalda ingredienser som harmoniskt kombineras
-        för att skapa en smakupplevelse utöver det vanliga.
-      </p>
-      <p>
-        Smeten till galetterna är gjord av bovete och vatten, helt glutenfri och
-        laktosfri medan crêpes -smeten blandas med vanlig mjölk, äkta ägg och
-        vetemjöl.
-      </p>
-      <p>Ta en titt på vår meny och välkommen till oss på crêperiet!</p>
-    </div>
+    <section className="screen-content">
+      <p>{t('title')}</p>
+      {about.map((p, i) => (
+        <p key={i} className="mb-6">
+          {t(p.text)}
+        </p>
+      ))}
+    </section>
   );
 };
 
