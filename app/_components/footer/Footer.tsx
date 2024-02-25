c6ac164ea995a0e24871e7864edac34e003612b4
Lee <leesheppard2404@gmail.com>
2024-02-25 18:07:50 +0100

    feat: Add Footer component

---
+++ app/_components/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useTranslations } from 'next-intl';
+
+const Footer = () => {
+  const t = useTranslations();
+
+  function getCurrentYear() {
+    return new Date().getFullYear();
+  }
+
+  const year = getCurrentYear();
+  return (
+    <footer className="screen-content text-center w-full p-4">
+      &#169; {year} - {t('copyright')}
+    </footer>
+  );
+};
+
+export default Footer;
