5b3944ca7385d835a97d79430c44334b1595010b
Lee <leesheppard2404@gmail.com>
2024-02-27 16:10:19 +0100

    refactor: Add SectionHeading component to About page

--- app/_components/about/About.tsx
+++ app/_components/about/About.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useTranslations } from 'next-intl';
 
 import { about } from '@/_assets/datasets/data';
+import SectionHeading from '../SectionHeading';
 
 const About = () => {
   const t = useTranslations('about');
   return (
     <section className="screen-content">
-      <p>{t('title')}</p>
+      <SectionHeading heading={'about.title'} />
       {about.map((p, i) => (
         <p key={i} className="mb-6">
           {t(p.text)}
