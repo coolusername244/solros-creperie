b4df5e0c4bc04650c27afe3d63c3158d58d48864
Lee <leesheppard2404@gmail.com>
2024-02-27 16:08:13 +0100

    Refactor ShowcaseImage component to support multiple heroes and dynamic grid layout

--- app/_components/ShowcaseImage.tsx
+++ app/_components/ShowcaseImage.tsx
@@ -1,33 +1,42 @@
 import React, { FC } from 'react';
 import Image from 'next/image';
 import { Hero } from '@/_types/types';
+import { useTranslations } from 'next-intl';
 
 type ShowcaseImageProps = {
-  hero: Hero;
-  key: number;
+  heros: Hero[];
+  cols: number;
 };
 
-const ShowcaseImage: FC<ShowcaseImageProps> = ({ hero, key }) => {
+const ShowcaseImage: FC<ShowcaseImageProps> = ({ heros, cols }) => {
+  const t = useTranslations();
+
   return (
     <div
-      key={key}
-      className={`relative h-96 ${
-        key !== 0 ? 'mt-12 lg:mt-0' : ''
-      } shadow-2xl rounded-md group overflow-hidden`}
+      className={`grid ${
+        heros.length >= 3 ? 'lg:grid-cols-3' : 'lg:grid-cols-2'
+      } gap-12 px-16 pb-16`}
     >
-      <Image
-        src={hero.image}
-        alt={hero.title}
-        layout="fill"
-        objectFit="cover"
-        loading="eager"
-        className="rounded-md group-hover:scale-110 transition-transform"
-      />
-      <div className="cursor-pointer absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 rounded-md hover:bg-opacity-50 transition duration-300">
-        <h2 className="text-white text-3xl font-bold tracking-wide group-hover:tracking-widest transition-all">
-          {hero.title}
-        </h2>
-      </div>
+      {heros.map((hero, i) => (
+        <div
+          key={i}
+          className={`relative h-96 shadow-2xl rounded-md group overflow-hidden`}
+        >
+          <Image
+            src={hero.image}
+            alt={t(hero.title)}
+            fill={true}
+            style={{ objectFit: 'cover' }}
+            loading="eager"
+            className="rounded-md group-hover:scale-110 transition-transform"
+          />
+          <div className="cursor-pointer absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 rounded-md hover:bg-opacity-50 transition duration-300">
+            <h2 className="text-white text-3xl font-bold tracking-wide group-hover:tracking-widest transition-all">
+              {t(hero.title)}
+            </h2>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
