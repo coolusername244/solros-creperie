6252a160ed0fa75c6da2b651a49e0151c151b031
Lee <leesheppard2404@gmail.com>
2024-03-01 18:01:03 +0100

    Update SectionHeading styling

--- app/_components/SectionHeading.tsx
+++ app/_components/SectionHeading.tsx
@@ -8,7 +8,7 @@ type SectionHeadingProps = {
 const SectionHeading: FC<SectionHeadingProps> = heading => {
   const t = useTranslations();
   return (
-    <div className="flex items-center justify-center py-12 text-3xl font-semibold">
+    <div className="flex items-center text-center justify-center py-12 text-3xl font-semibold">
       <h2>{t(heading.heading)}</h2>
     </div>
   );
