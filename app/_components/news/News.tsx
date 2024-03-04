e0f7caa3eef8b0ed09176ec766adc7846a4f629f
Lee <leesheppard2404@gmail.com>
2024-03-04 15:43:32 +0100

    refactor: added video placeholder

--- app/_components/news/News.tsx
+++ app/_components/news/News.tsx
@@ -8,6 +8,9 @@ const News = () => {
     <section>
       <SectionHeading heading={'news.title'} />
       <ShowcaseImage heros={newsHeros} />
+      <div className="w-[75vw] m-auto h-96 mb-12 justify-center items-center flex text-white text-4xl uppercase text-center bg-slate-600">
+        <p>video will go here</p>
+      </div>
     </section>
   );
 };
