0abb433d40bc2fef5292f2622e7f5992c4202dc0
Lee <leesheppard2404@gmail.com>
2024-03-04 14:47:29 +0100

    Add coffee image to Offers component

--- app/_components/offers/Offers.tsx
+++ app/_components/offers/Offers.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import ShowcaseImage from '@/_components/ShowcaseImage';
 import SectionHeading from '../SectionHeading';
 import { offersHeros } from '@/_assets/datasets/data';
+import Image from 'next/image';
+import coffee from '@/_assets/images/drinks/coffee/coffee-4.jpg';
 
 const Offers = () => {
   return (
     <section className="light-mint">
       <SectionHeading heading={'offers.title'} />
       <ShowcaseImage heros={offersHeros} />
+      {/* <Image src={coffee} alt="" /> */}
+      <div className="w-[75vw] m-auto pb-12">
+        <Image
+          src={coffee}
+          alt={'coffee'}
+          // fill={true}
+          style={{ objectFit: 'cover' }}
+          loading="eager"
+          className="w-full h-96"
+        />
+      </div>
     </section>
   );
 };
