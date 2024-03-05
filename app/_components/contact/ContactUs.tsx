369d7ff697d92d81a47d1700223be079ea2d26bb
Lee <leesheppard2404@gmail.com>
2024-03-05 15:12:15 +0100

    Refactor: updated classes for openhours and contact

--- app/_components/contact/ContactUs.tsx
+++ app/_components/contact/ContactUs.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 import { contactMethods } from '@/_assets/datasets/data';
 
 const ContactUs = () => {
-  const t = useTranslations('footer');
+  const t = useTranslations('contact');
 
   return (
-    <section className="footer-section lg:basis-1/3">
-      <div className="footer-heading">
+    <section className="contact-section">
+      <div className="contact-heading">
         <h5>{t('contact-us')}</h5>
       </div>
       <ul className="space-y-3">
         {contactMethods.map((contact, i) => (
-          <li key={i} className="flex lg:justify-center">
-            <Link href={contact.href} className="footer-text flex items-center">
+          <li key={i}>
+            <Link href={contact.href} className="flex justify-center">
               <span className="text-xl">{contact.icon}</span>
               <span className="ml-3">{contact.contact}</span>
             </Link>
