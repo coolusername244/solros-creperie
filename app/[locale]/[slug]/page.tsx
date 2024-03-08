4ad47172df4320bb1fa1ec48205bb8e574ffd395
Lee <leesheppard2404@gmail.com>
2024-03-08 16:33:15 +0100

    Add MenuCategoryItems component

---
+++ app/[locale]/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuCategoryItems = () => {
+  return <main className="mt-[100px] lg:mt-[130px]">MenuCategoryItems</main>;
+};
+
+export default MenuCategoryItems;
