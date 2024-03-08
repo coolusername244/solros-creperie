049376bb5b6e997b9f86796a764d246f99410ed9
Lee <leesheppard2404@gmail.com>
2024-03-08 16:28:32 +0100

    refactor: moved navbar and footer to layout

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,21 +1,17 @@
-import Navbar from '@/components/navbar/Navbar';
 import Menu from '@/_components/menu/Menu';
 import News from '@/_components/news/News';
 import Offers from '@/_components/offers/Offers';
 import About from '@/_components/about/About';
 import Contact from '@/_components/contact/Contact';
-import Footer from '@/_components/footer/Footer';
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Menu />
       <Offers />
       <News />
       <About />
       <Contact />
-      <Footer />
     </main>
   );
 }
