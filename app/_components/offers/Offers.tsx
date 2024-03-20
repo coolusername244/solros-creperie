a70f9e9e2cffbdf949c79e4608c872c293aa9e71
Lee <leesheppard2404@gmail.com>
2024-03-20 13:57:47 +0100

    refacator: removed hero image from offers section

--- app/_components/offers/Offers.tsx
+++ app/_components/offers/Offers.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import ShowcaseImage from '@/_components/ShowcaseImage';
 import SectionHeading from '../SectionHeading';
 import { offersHeros } from '@/_assets/datasets/data';
-import Image from 'next/image';
-import coffee from '@/_assets/images/drinks/coffee/coffee-4.jpg';
 
 const Offers = () => {
   return (
     <section id="offers" className="light-mint">
       <SectionHeading heading={'offers.title'} />
       <ShowcaseImage heros={offersHeros} />
-      <div className="w-[75vw] m-auto pb-12">
-        <Image
-          src={coffee}
-          alt={'coffee'}
-          style={{ objectFit: 'cover' }}
-          loading="lazy"
-          sizes="90vw"
-          priority={false}
-          className="w-full h-96"
-        />
-      </div>
     </section>
   );
 };
