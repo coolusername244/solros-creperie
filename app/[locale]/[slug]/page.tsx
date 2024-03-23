16e32f8c8a18cdd19ed12d31717d6a2da30d20e9
Lee <leesheppard2404@gmail.com>
2024-03-23 19:42:49 +0100

    refactor: removed functions that are now placed in helper.ts

--- app/[locale]/[slug]/page.tsx
+++ app/[locale]/[slug]/page.tsx
@@ -1,90 +1,30 @@
+'use client';
 import React from 'react';
-import Image from 'next/image';
-import { useTranslations } from 'next-intl';
 
-import {
-  galetteItems,
-  crepeItems,
-  toastItems,
-  kidsItems,
-  coffeeTeaItems,
-  otherItems,
-} from '@/_assets/datasets/data';
-import { MenuItem } from '@/_types/types';
+import { getItems, getQuery } from '../../../helpers';
+import FoodMenuLayout from '@/_components/FoodMenuLayout';
+import DrinkMenuLayout from '@/_components/DrinkMenuLayout';
 
 const MenuCategoryItems = ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
 
-  let query = slug;
+  const items = getItems(slug);
+  const query = getQuery(slug);
 
-  if (slug === 'galetter') {
-    query = 'galettes';
-  }
-  if (slug === 'sota-crepes') {
-    query = 'sweet-crepes';
-  }
-  if (slug === 'toasts-och-paninis') {
-    query = 'toasts-and-paninis';
-  }
-  if (slug === 'barn-meny') {
-    query = 'kids-menu';
-  }
-  if (slug === 'kaffe-och-te') {
-    query = 'coffee-and-tea';
-  }
-  if (slug === 'ovriga-drycker') {
-    query = 'other-drinks';
+  let foodOrDrink;
+
+  if (query === 'coffee-and-tea') {
+    foodOrDrink = 'drink';
+  } else if (query === 'other-drinks') {
+    foodOrDrink = 'drink';
+  } else {
+    foodOrDrink = 'food';
   }
 
-  const t = useTranslations(`food.${query}`);
-
-  let items: MenuItem[] = [];
-
-  switch (slug) {
-    case 'galetter':
-    case 'galettes':
-      items = galetteItems;
-      break;
-    case 'sota-crepes':
-    case 'sweet-crepes':
-      items = crepeItems;
-      break;
-    case 'toasts-och-paninis':
-    case 'toasts-and-paninis':
-      items = toastItems;
-      break;
-    case 'barn-meny':
-    case 'kids-menu':
-      items = kidsItems;
-      break;
-    case 'kaffe-och-te':
-    case 'coffee-and-tea':
-      items = coffeeTeaItems;
-      break;
-    case 'ovriga-drycker':
-    case 'other-drinks':
-      items = otherItems;
-      break;
-  }
-
-  return (
-    <main className="mt-[100px] lg:mt-[130px]">
-      {items.map((item, i) => (
-        <>
-          <p key={i}>{t(item.title)}</p>
-          {item.description && <p>{t(item.description)}</p>}
-          {item.price && <p>{t(item.price)}</p>}
-          {item.image && (
-            <Image
-              src={item.image.src}
-              alt={item.title}
-              width={100}
-              height={100}
-            />
-          )}
-        </>
-      ))}
-    </main>
+  return foodOrDrink === 'food' ? (
+    <FoodMenuLayout slug={slug} items={items} />
+  ) : (
+    <DrinkMenuLayout slug={slug} items={items} />
   );
 };
 
