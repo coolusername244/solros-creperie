87410a6929d71e5daa11587a682f9579313323c9
Lee <leesheppard2404@gmail.com>
2024-03-26 20:35:11 +0100

    Add support for decoding slugs and handling additional slug values

--- helpers.ts
+++ helpers.ts
@@ -27,25 +27,29 @@ export const handleClick = (name: string) => {
   }
 };
 
+export const decodeSlug = (slug: string): string => {
+  return decodeURIComponent(slug);
+};
+
 export const getQuery = (slug: string) => {
   let query = slug;
 
-  if (slug === 'galetter') {
+  if (slug === 'galetter' || slug === 'галети') {
     query = 'galettes';
   }
-  if (slug === 'sota-crepes') {
+  if (slug === 'sota-crepes' || slug === 'солодкий-млинці') {
     query = 'sweet-crepes';
   }
-  if (slug === 'toasts-och-paninis') {
+  if (slug === 'toasts-och-paninis' || slug === 'тости-та-паніні') {
     query = 'toasts-and-paninis';
   }
-  if (slug === 'barn-meny') {
+  if (slug === 'barn-meny' || slug === 'дитяче-меню') {
     query = 'kids-menu';
   }
-  if (slug === 'kaffe-och-te') {
+  if (slug === 'kaffe-och-te' || slug === 'кава-та-чай') {
     query = 'coffee-and-tea';
   }
-  if (slug === 'ovriga-drycker') {
+  if (slug === 'ovriga-drycker' || slug === 'інші-напої') {
     query = 'other-drinks';
   }
 
@@ -58,6 +62,7 @@ export const getItems = (slug: string) => {
   switch (slug) {
     case 'galetter':
     case 'galettes':
+    case 'галети':
       items = galetteItems;
       break;
     case 'sota-crepes':
