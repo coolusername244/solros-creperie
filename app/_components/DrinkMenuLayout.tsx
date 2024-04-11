25abc00d4d4a05403c2215d04c4f1d030f83993d
Lee <leesheppard2404@gmail.com>
2024-04-11 20:02:42 +0200

    Update descriptions for coffee and tea menu items

--- app/_components/DrinkMenuLayout.tsx
+++ app/_components/DrinkMenuLayout.tsx
@@ -4,20 +4,35 @@ import PhotoAlbum, { Photo } from 'react-photo-album';
 
 import { MenuLayoutProps } from '@/_types/types';
 import { getQuery } from '../../helpers';
-import { coffeeImages, otherDrinkImages } from '@/_assets/datasets/data';
+import {
+  coffeeImages,
+  otherDrinkImages,
+  coffeeTeaDescription,
+  otherDrinkDescription,
+} from '@/_assets/datasets/data';
 
 const DrinkMenuLayout: FC<MenuLayoutProps> = ({ slug, items }) => {
   let query = getQuery(slug);
   const t = useTranslations(`food.${query}`);
 
   let collageImages: Photo[] = [];
+  let description: { text: string }[] = [];
 
   slug === 'coffee-and-tea'
-    ? (collageImages = coffeeImages)
-    : (collageImages = otherDrinkImages);
+    ? ((collageImages = coffeeImages), (description = coffeeTeaDescription))
+    : ((collageImages = otherDrinkImages),
+      (description = otherDrinkDescription));
 
+  console.log('description', description);
   return (
     <main className="mt-[100px] lg:mt-[130px] flex h-fit">
+      <section className="w-full">
+        {description.map((p, i) => (
+          <p key={i} className="px-4 py-2">
+            {t(p.text)}
+          </p>
+        ))}
+      </section>
       <section className="flex flex-col justify-evenly items-end text-right basis-1/3">
         {items.map((item, i) => (
           <div key={i} className="border-b w-full border-slate-500 mr-6">
