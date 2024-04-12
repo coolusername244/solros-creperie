ed9608819486209c586439399f14c1d220c93679
Lee <leesheppard2404@gmail.com>
2024-04-12 14:12:07 +0200

    Update FindUs component to use responsive iframe dimensions

--- app/_components/contact/FindUs.tsx
+++ app/_components/contact/FindUs.tsx
@@ -6,18 +6,8 @@ const FindUs = () => {
       <div className="flex justify-center">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2035.838741049541!2d18.062087677271304!3d59.318949674609314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f773064ce7121%3A0x47761528516fb25f!2sSolros%20Cr%C3%AAperie!5e0!3m2!1sen!2sse!4v1709643611416!5m2!1sen!2sse"
-          width="400"
-          height="300"
           title="map of Solros Crêperie"
-          className="w-[90%] lg:hidden"
-          loading="lazy"
-        ></iframe>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2035.838741049541!2d18.062087677271304!3d59.318949674609314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f773064ce7121%3A0x47761528516fb25f!2sSolros%20Cr%C3%AAperie!5e0!3m2!1sen!2sse!4v1709563499173!5m2!1sen!2sse"
-          width="600"
-          height="450"
-          title="map of Solros Crêperie"
-          className=" hidden lg:block"
+          className="w-[80vw] h-[40vh]"
           loading="lazy"
         ></iframe>
       </div>
